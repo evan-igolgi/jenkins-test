10f4fbb96f8c47f8ad77576cf04f62af72dddb54
evan <evan@evan-office>
2018-07-23 10:53:40 -0500

    Added Dockerfile and changed test

--- test.cxx
+++ test.cxx
@@ -6,7 +6,7 @@ int main()
 
     std::ofstream myfile;
     myfile.open("example.txt");
-    myfile << "First Name=Eva\n";
+    myfile << "First Name=Evan\n";
     myfile << "Last Name=Dotterer";
     myfile.close();
     return 0;
